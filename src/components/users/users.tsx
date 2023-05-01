e080faa342288035e307b41eb56264baa7df8209
JoshuaAyodele <jayodele@getreliancehealth.com>
2023-05-01 19:48:40 +0100

    mock hhtp request

--- src/components/users/users.tsx
+++ src/components/users/users.tsx
@@ -4,7 +4,7 @@ export const Users = () => {
   const [users, setUsers] = useState<string[]>([]);
   const [error, setError] = useState<string | null>(null);
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode/users")
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((data) => setUsers(data.map((user: { name: string }) => user.name)))
       .catch(() => setError("Error fetching users"));
